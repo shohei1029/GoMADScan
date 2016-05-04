55021f9b13b47d229107d33cd773b3a6533612b6
Carushi <trumpet-lambda@hotmail.co.jp>
2016-05-04 14:48:37 +0900

    add install script

---
+++ install/install_mac.sh
@@ -0,0 +1,10 @@
+brew install git
+brew install go
+brew install pkgconfig
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+brew install gtk+
+brew install gtksourceview
+
+mkdir ~/go
+export GOPATH=~/go
+go get github.com/carushi/GoMADScan
